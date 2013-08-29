e1872e05a219a218ce0ebb73df1f5efe1a130597
iliapolo <elip@gigaspaces.com>
2013-08-29 15:38:32 +0300

    CLOUDIFY-2012 added python_webserver_installer project

---
+++ setup.py
@@ -0,0 +1,19 @@
+__author__ = 'elip'
+
+from setuptools import setup
+
+setup(
+    zip_safe=True,
+    name='cosmo-python-webserver',
+    version='0.1.0',
+    author='elip',
+    author_email='elip@gigaspaces.com',
+    packages=['python_webserver_installer'],
+    license='LICENSE',
+    description='Plugin for starting a simple python webserver on the localhost',
+    install_requires=[
+        "billiard==2.7.3.28",
+        "celery==3.0.19",
+    ],
+    tests_require=['nose']
+)
