0f4881401db9189c64a2f61beb330e984a353523
iliapolo <elip@gigaspaces.com>
2013-09-02 12:22:55 +0300

    CLOUDIFY-2012 move dependencies back to setup.py

--- setup.py
+++ setup.py
@@ -10,5 +10,8 @@ setup(
     packages=['python_webserver_installer'],
     license='LICENSE',
     description='Plugin for starting a simple python webserver on the localhost',
+    install_requires=[
+        "celery==3.0.19"
+    ],
     tests_require=['nose']
 )
