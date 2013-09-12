f078ed88e31c7d931096c1fbc82bcbb104a493e3
iliapolo <elip@gigaspaces.com>
2013-09-12 11:17:30 +0300

    CLOUDIFY-2022 refactor branch name and export from travis

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
+import os
+
 __author__ = 'elip'
 
 from setuptools import setup
+
+DEFAULT_BRANCH = "feature/CLOUDIFY-2022-initial-commit"
+
+BRANCH = os.environ.get("COSMO_BRANCH", DEFAULT_BRANCH)
+
+COSMO_CELERY = "https://github.com/CloudifySource/cosmo-celery-common/tarball/{0}".format(BRANCH)
+COSMO_CELERY_VERSION = "0.1.0"
+
 
 setup(
     name='cosmo-plugin-python-webserver',
@@ -11,6 +21,8 @@ setup(
     license='LICENSE',
     description='Plugin for starting a simple python webserver on the localhost',
     install_requires=[
+        "cosmo-celery-common",
         "celery"
-    ]
+    ],
+    dependency_links=["{0}#egg=cosmo-celery-common-{1}".format(COSMO_CELERY, COSMO_CELERY_VERSION)]
 )
