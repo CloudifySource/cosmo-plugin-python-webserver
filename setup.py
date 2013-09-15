7ddcb2278228c4103235bf68c21c1a17066c9107
iliapolo <elip@gigaspaces.com>
2013-09-15 14:36:32 +0300

    CLOUDIFY-2022 point cosmo common dependency to master branch

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
-import os
-
 __author__ = 'elip'
 
 from setuptools import setup
 
-DEFAULT_BRANCH = "feature/CLOUDIFY-2022-initial-commit"
+DEFAULT_BRANCH = "master"
 
-BRANCH = os.environ.get("COSMO_BRANCH", DEFAULT_BRANCH)
-
-COSMO_CELERY = "https://github.com/CloudifySource/cosmo-celery-common/tarball/{0}".format(BRANCH)
+COSMO_CELERY = "https://github.com/CloudifySource/cosmo-celery-common/tarball/master"
 COSMO_CELERY_VERSION = "0.1.0"
 
 
@@ -21,8 +17,7 @@ setup(
     license='LICENSE',
     description='Plugin for starting a simple python webserver on the localhost',
     install_requires=[
-        "cosmo-celery-common",
-        "celery"
+        "cosmo-celery-common"
     ],
     dependency_links=["{0}#egg=cosmo-celery-common-{1}".format(COSMO_CELERY, COSMO_CELERY_VERSION)]
 )
