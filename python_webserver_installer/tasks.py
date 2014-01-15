c55b662df2997ca0e01e88a4d0b97bc6afb6c9bc
idanmo <idan@gigaspaces.com>
2014-01-15 11:36:14 +0200

    serve default index.html and added test

--- python_webserver_installer/tasks.py
+++ python_webserver_installer/tasks.py
@@ -1,43 +1,45 @@
-#/*******************************************************************************
-# * Copyright (c) 2013 GigaSpaces Technologies Ltd. All rights reserved
-# *
-# * Licensed under the Apache License, Version 2.0 (the "License");
-# * you may not use this file except in compliance with the License.
-# * You may obtain a copy of the License at
-# *
-# *       http://www.apache.org/licenses/LICENSE-2.0
-# *
-# * Unless required by applicable law or agreed to in writing, software
-# * distributed under the License is distributed on an "AS IS" BASIS,
+########
+# Copyright (c) 2014 GigaSpaces Technologies Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
 #    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    * See the License for the specific language governing permissions and
 #    * limitations under the License.
-# *******************************************************************************/
 
 """
-A celery task for starting a simple http server using a python command.
+Cloudify plugin for running a Python simple HTTP server.
+
+Operations:
+    configure: Creates an index.html file.
+    start: Starts the server
 """
 
 import time
 import urllib2
 import os
-
+import tempfile
 from celery import task
-
-from cosmo.events import send_event
-
-
-@task
-def install(**kwargs):
-    pass
+from cosmo.events import send_event as send_riemann_event
+from cloudify.utils import get_local_ip
 
 
-@task
-def start(__cloudify_id, port=8080, **kwargs):
-    os.system("nohup python -m SimpleHTTPServer {0} &".format(port))
+get_ip = get_local_ip
+send_event = send_riemann_event
 
-    # verify http server is up
-    for attempt in range(10):
+
+def get_webserver_root():
+    return os.path.join(tempfile.gettempdir(), 'python-simple-http-webserver')
+
+
+def verify_http_server(port=8080):
+    for attempt in range(15):
         try:
             response = urllib2.urlopen("http://localhost:{0}".format(port))
             response.read()
@@ -46,4 +48,31 @@ def start(__cloudify_id, port=8080, **kwargs):
             time.sleep(1)
     else:
         raise RuntimeError("failed to start python http server")
-    send_event(__cloudify_id, "10.0.0.5", "webserver status", "state", "running")
+
+
+@task
+def configure(__cloudify_id, **kwargs):
+    os.system('mkdir -p {0}'.format(get_webserver_root()))
+    html = """
+<html>
+    <header>
+        <title>Cloudify Hello World</title>
+    </header>
+<body>
+    <h1>Hello, World!</h1>
+    <p>node_id = {0}</p>
+</body>
+</html>
+    """.format(__cloudify_id)
+    html_file = os.path.join(get_webserver_root(), 'index.html')
+    if not os.path.exists(html_file):
+        with open(html_file, 'w') as f:
+            f.write(html)
+
+
+@task
+def start(__cloudify_id, port=8080, **kwargs):
+    os.system("cd {0}; nohup python -m SimpleHTTPServer {1} &".format(get_webserver_root(), port))
+    verify_http_server(port)
+    send_event(__cloudify_id, get_ip(), "webserver status", "state", "running")
+
